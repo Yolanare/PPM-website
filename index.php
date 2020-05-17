e4c7a3a4527ef387244417d2d8fd662029919362
Yolanare <y09a3115@gmail.com>
2020-05-17 19:54:54 +0200

    mobile optimisation + qlques trucs

--- index.php
+++ index.php
@@ -1,72 +1,22 @@
-<?php $page = "accueil"; ?>
+<?php $page = "accueil"; $path = $_SERVER['DOCUMENT_ROOT']."/PPM-website/"; ?>
 <!DOCTYPE html>
 
 <html>
 <head>
     <title>Accueil - Pamplemousse</title>
     
-    <?php include 'includes/head.php'; ?>
+    <?php include $path.'includes/head.php'; ?>
     <style type="text/css">
-        img[id*="homew"] {
-            position: fixed;
-        }
 
-        #homew_tr {
-            top: 0;
-            right: 0;
-            width: 35%;
-        }
-        #homew_br {
-            bottom: 0;
-            right: 0;
-            width: 80%;
-        }
-
-        #welcome_center {    
-            height: 100%;
-            display: flex;
-            justify-content: center;
-        }
-        .welcome {
-            margin-top: 20%;
-            margin-right: 25%;
-            position: relative;
-            height: min-content;
-        }
-        #w1 {
-            font-size: 35px;
-            text-align: left;
-            color: var(--accent-fruit);
-        }
-        #w2 {
-            margin-top: 10px;
-            font-size: 20px;
-            text-align: left;
-            opacity: 0.5;
-            color: var(--accent-fruit);
-        }
     </style>
 </head>
 <body>
     <!-- NAVIGATION -->
-    <?php include 'includes/nav.php'; ?>
+    <?php include $path.'includes/nav.php'; ?>
 
     <!-- CONTENT -->
-    <div id="content" style="align-items: center;">
-        <div class="homew">
-            <img id="homew_tr" src="src/homew_tr.png" />
-            <img id="homew_br" src="src/homew_br.png" />
-        </div>
-        <div id="welcome_center">
-            <div class="welcome">
-                <div id="w1">Bienvenue sur le site!</div>
-                <div id="w2">
-                    Dirige toi vers ce qui t'intéresse à l'aide du menu à ta disposion.<br>
-                    <i>(Cette page changera plus tard pour glorifier l'équipe)</i><br><br>
-                    On est en putain de bêta de toute façon kestufoulà maggle wsh tarien àfaire ic i  !??!
-                </div>
-            </div>
-        </div>
+    <div id="content">
+
     </div>
 
 
--- ppmscript.js
+++ ppmscript.js
@@ -5,42 +5,75 @@ var nav_logo = document.querySelector("#navtop_ppm");
 var navlarge_ppm = document.querySelector("#navlarge_ppm");
 var nav_show_btn = document.querySelector("#mobile_open_close_btn");
 var nav_darkbg = document.querySelector("#mobile_nav_dark_bg");
-
+var nav_current = document.querySelector(".current");
 
 // MISC
-var currentScrollPosPercent = function getScrollPercent() { // https://stackoverflow.com/a/8028584
+var currentScrollPosPercent = function getScrollPercent() {
+    // https://stackoverflow.com/a/8028584
     var h = document.documentElement,
         b = document.body,
-        st = 'scrollTop',
-        sh = 'scrollHeight';
-    return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight); //  * 100
+        st = "scrollTop",
+        sh = "scrollHeight";
+    return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight); // * 100
+};
+
+// SPAWN
+if (window.innerWidth > 901) {
+    // current
+    if (nav_current != null) {
+        nav_current.style.transition = "none";
+        nav_current.classList.add("current_spawn");
+        setTimeout(function () {
+            nav_current.style.transition = "2s cubic-bezier(0.6, 0, 0, 1)";
+        }, 0);
+        setTimeout(function () {
+            nav_current.classList.remove("current_spawn");
+        }, 0);
+        setTimeout(function () {
+            nav_current.style.transition = null;
+        }, 1000);
+    }
+
+    // navlarge_ppm
+    navlarge_ppm.classList.add("navlarge_ppm_focus");
+    setTimeout(function () {
+        navlarge_ppm.style.transition = "1s cubic-bezier(0.5, 0, 0, 1)";
+    }, 0);
+    setTimeout(function () {
+        navlarge_ppm.classList.remove("navlarge_ppm_focus");
+    }, 0);
 }
 
+nav_focus = function () {
+    if (window.innerWidth > 901) {
+        navlarge_ppm.classList.add("navlarge_ppm_focus");
+    }
+};
 
 // -- MOBILE
 // MENU - Auto-hide navigation bar
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+function NavBarAutoHide() {
     if (window.innerWidth < 900) {
         var currentScrollPos = window.pageYOffset;
         if (nav_show_btn.getAttribute("roulemapoule") == "0") {
             if (prevScrollpos > currentScrollPos) {
                 nav.style.top = "0";
             } else {
-                nav.style.top = "calc(-1px + (0vw - var(--nav_nav_show_btn_height)))";
+                nav.style.top = "calc(-1px + (0vw - var(--nav_show_btn_height)))";
             }
             prevScrollpos = currentScrollPos;
-        }
-        else {
+        } else {
             nav.style.top = "0";
         }
     }
 }
-
+document.addEventListener("scroll", NavBarAutoHide);
 
 // MENU - Folding navigation
-nav_show = function() {
-    if (nav_show_btn.getAttribute("roulemapoule") == "0") { // shown
+nav_show = function () {
+    if (nav_show_btn.getAttribute("roulemapoule") == "0") {
+        // shown
         document.querySelector("body").style.overflow = "hidden";
         nav_show_btn.setAttribute("roulemapoule", "1");
         navlarge_ppm.style.opacity = "1";
@@ -49,16 +82,17 @@ nav_show = function() {
         nav.style.transform = "translateY(0%)";
 
         nav_logo.style.animation = "none";
-        setTimeout(function() { nav_logo.style.animation = "mobile_navtop_ppm_spawn 1.3s cubic-bezier(0.4, 0, 0, 1)"; },0) // yeah i'm a genius for using that like that
+        setTimeout(function () {
+            nav_logo.style.animation = "mobile_navtop_ppm_spawn 1.5s cubic-bezier(0.4, 0, 0, 1)";
+        }, 0);
         nav_menu.style.top = "55%";
         nav_menu.style.transition = "1.15s cubic-bezier(0.7, 0, 0, 1)";
 
-        nav_darkbg.style.transition = "transform 0.8s cubic-bezier(0.6, 0, 0, 1), opacity 1.75s cubic-bezier(0.3, 0.1, 0, 1)";
-        nav_darkbg.style.transform = "translateY(5%)";
+        nav_darkbg.style.transition = "opacity 1.4s cubic-bezier(0.5, 0, 0, 1)";
         nav_darkbg.style.opacity = "0.4";
         nav_darkbg.style.pointerEvents = "auto";
-    }
-    else { // hidden
+    } else {
+        // hidden
         document.querySelector("body").style.overflow = "auto";
 
         nav_show_btn.setAttribute("roulemapoule", "0");
@@ -70,22 +104,8 @@ nav_show = function() {
         nav_menu.style.top = "45%";
         nav_menu.style.transition = "0s 1s";
 
-        nav_darkbg.style.transition = "transform 0s 1.15s, opacity 1.1s cubic-bezier(0.7, 0, 0, 1)"; // cubic-bezier(0.6, 0, 0, 1)
-        nav_darkbg.style.transform = "translateY(-105%)";
+        nav_darkbg.style.transition = "opacity 1.3s cubic-bezier(0.6, 0, 0, 1)"; // cubic-bezier(0.6, 0, 0, 1)
         nav_darkbg.style.opacity = "0";
         nav_darkbg.style.pointerEvents = "none";
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
