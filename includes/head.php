3531d313470a2bae26815999d754382443bd9199
Yolanare <y09a3115@gmail.com>
2020-05-17 19:06:55 +0200

    compatibilité avec technique dossier + mini modifs

--- includes/head.php
+++ includes/head.php
@@ -6,9 +6,8 @@
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
-<link rel="icon" type="image/png" sizes="32x32" href="src/icons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="src/icons/favicon-16x16.png">
-<link rel="mask-icon" href="src/icons/safari-pinned-tab.svg" color="#ff3875">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php if($page!="accueil"){echo "../";} ?>src/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php if($page!="accueil"){echo "../";} ?>src/icons/favicon-16x16.png">
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap">
-<link rel="stylesheet" href="ppmstyle.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600&display=swap">
+<link rel="stylesheet" href="<?php if($page!="accueil"){echo "../";} ?>ppmstyle.css">
--- includes/nav.php
+++ includes/nav.php
@@ -1,21 +1,20 @@
 <div id="navigation">
-    <div id="mobile_nav_dark_bg" onclick="nav_show()" style="opacity: 0;"></div>
+    <div id="mobile_nav_dark_bg" onclick="nav_show()"></div>
     <nav>
         <div id="navtop_ppm">
-            <a href="index.php"></a>
-            <img src="src/ppm300.png" />
+            <a onclick="nav_focus()" href="<?php if($page!="accueil"){echo "../";} ?>"></a>
+            <img src="<?php if($page!="accueil"){echo "../";} ?>src/ppm300.png" />
         </div>
         <div id="menu">
             <div class="menu_btn">
-                <!--<a class="mbtns" id="mbtn-home" href="home.html">Accueil</a>-->
-                <a class="mbtns <?php if($page=="projets"){echo "current";} ?>" id="mbtn-projets" href="projets.php">Nos Projets</a>
-                <a class="mbtns <?php if($page=="membres"){echo "current";} ?>" id="mbtn-membres" href="membres.php">Nos Membres</a>
-                <a class="mbtns <?php if($page=="contact"){echo "current";} ?>" id="mbtn-contact" href="contact.php">Nous Contacter</a>
+                <a class="mbtns <?php if($page=="projets"){echo"current";} ?>" id="mbtn-projets" onclick="nav_focus()" href="<?php if($page!="accueil"){echo"../";} ?>projets/">Nos Projets</a>
+                <a class="mbtns <?php if($page=="membres"){echo"current";} ?>" id="mbtn-membres" onclick="nav_focus()" href="<?php if($page!="accueil"){echo"../";} ?>membres/">Nos Membres</a>
+                <a class="mbtns <?php if($page=="contact"){echo"current";} ?>" id="mbtn-contact" onclick="nav_focus()" href="<?php if($page!="accueil"){echo"../";} ?>contact/">Nous Contacter</a>
             </div>
         </div>
         <div id="mobile_open_close_btn" onclick="nav_show()" roulemapoule="0"></div>
         <div id="navlarge_ppm">
-            <img id="ppm" src="src/ppmnavlarge.png" />
+            <img src="<?php if($page!="accueil"){echo "../";} ?>src/ppmnavlarge.png" />
         </div>
     </nav>
 </div>
