5d324260ad24595dc5c6dc29cbd788e3dbe5d0a6
Yolanare <y09a3115@gmail.com>
2020-05-21 12:26:25 +0200

    boutons footer tr

--- index.php
+++ index.php
@@ -128,6 +128,9 @@
         }
 
         #foot_links {
+            display: flex;
+            justify-content: center;
+            align-items: center;
             margin-top: 3.5%;
             text-align: center;
         }
@@ -136,13 +139,26 @@
             position: relative;
             width: 70px;
             height: 70px;
-            margin: 0 30px;
+            margin: 0 25px;
+            transform: scale(1.00001);
             border-radius: 9999px;
-            transition: background-color 1s cubic-bezier(0.2, 0.5, 0, 1);
+            transition: color 0.5s cubic-bezier(0.3, 0.4, 0, 1),
+                background-color 1s cubic-bezier(0.2, 0.5, 0, 1),
+                transform 0.5s cubic-bezier(0.1, 0.4, 0, 1) !important;
         }
         .foot_link:hover {
+            transform: scale(1.175);
             background-color: var(--accent-fruit) !important;
-            transition: 0.7s cubic-bezier(0.1, 0.5, 0, 1);
+            transition: 1s cubic-bezier(0.1, 0.6, 0, 1),
+                transform 0.35s cubic-bezier(0.2, 0.4, 0, 1.85) !important;
+        }
+        .foot_link:hover > div {
+            transform: rotate(360deg) !important;
+            transition: transform 0.6s cubic-bezier(0.2, 0.4, 0, 1) !important;
+        }
+        .foot_link:active > div {
+            transform: scale(0.9) !important;
+            transition: transform 0.2s cubic-bezier(0, 0.75, 0, 1) !important;
         }
 
         .foot_link > div {
@@ -152,14 +168,13 @@
             display: flex !important;
             justify-content: center;
             align-items: center;
+            transform: rotate(0deg) !important;
             border-radius: inherit;
+            transition: transform 1s cubic-bezier(0.5, 0.4, 0, 1) !important;
         }
-        .btn_fl_tr a div svg {
+        .foot_link div svg {
             width: 55%;
             fill: var(--bg);
-        }
-        .btn_fl_tr a:hover > div {
-            transform: scale(0.875);
         }
 
 
@@ -204,7 +219,7 @@
                 <div id="foot_title">
                     Les coins où nous retrouver!
                 </div>
-                <div class="btn1_tr btn_fl_tr" id="foot_links">
+                <div class="btn1_tr" id="foot_links">
                     <?php $footLinks = array(
                         $pmc =array( "color" => "#6EC310", "link" => "https://www.planetminecraft.com/member/pamplemousse/", "size" => "126 126", "logo" => "M9,81V45h9V27h9v-9h18V9h36v9h18v9h9v18h9v36h-9v9v9h-9v9H81v9H45v-9H27v-9h-9V81H9z M36,36v18h18V36H36z" ),
                         $twitter = array( "color" => "#29A9ED", "link" => "https://twitter.com/PamplemousseBT", "size" => "24 24", "logo" => "M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z" ),
